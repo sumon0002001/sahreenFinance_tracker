c070a205cde2590d3c6d46f46d8bd8ecd3b69f16
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-05-05 19:52:56 +0600

    Add form and function without dispaly data

--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -1,11 +1,30 @@
+import { useUser } from "@clerk/clerk-react";
 import React, { useState } from "react";
 
 const FinancialRecordForm = () => {
-  const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [paymentMethod, setPaymentMethod] = useState<string>("");
+
+  const { user } = useUser();
+
+  const handleSubmit = (e: React.FocusEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Perform form submission logic here
+
+    const newRecord = {
+      userId: user?.id,
+      date: new Date().toISOString(),
+      description: description,
+      amount: parseFloat(amount),
+      category: category,
+      paymentMethod: paymentMethod,
+    };
+  };
   return (
     <div className="form-container">
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <div className="form-field">
           <label htmlFor="">Description</label>
           <input
@@ -28,6 +47,42 @@ const FinancialRecordForm = () => {
             value={amount}
           />
         </div>
+        <div className="form-field">
+          <label>Category: </label>
+          <select
+            required
+            name="category"
+            className="input"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value=""> Select a Category</option>
+            <option value="food">Food</option>
+            <option value="Rent">Rent</option>
+            <option value="Salary">Salary</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className="form-field">
+          <label htmlFor="">Payment Method</label>
+          <select
+            required
+            name="payment-method"
+            className="input"
+            value={paymentMethod}
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          >
+            <option value="">Select a Payment Method</option>
+            <option value="Credit Card">Credit Card</option>
+            <option value="Cash">Cash</option>
+            <option value="Bank Transfer">Bank Transfer</option>
+          </select>
+        </div>
+        <button type="submit" className="button">
+          Add Record
+        </button>
       </form>
     </div>
   );
