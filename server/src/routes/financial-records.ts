e167e64a74025e689cc7c0ceeea09cd2b6d2c400
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-05-07 15:55:04 +0600

    Add end point to the router

--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -18,6 +18,46 @@ router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
   }
 });
 
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const newBodyRecord = req.body;
+    const NewRecord = new FinancialRecordModel(newBodyRecord);
+    const savedRecord = NewRecord.save();
+    res.status(201).send(savedRecord);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const newRecordBody = req.body;
+    const record = await FinancialRecordModel.findByIdAndUpdate(
+      id,
+      newRecordBody,
+      { new: true }
+    );
+    if (!record) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+    res.status(200).send(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const record = await FinancialRecordModel.findByIdAndDelete(id);
+    if (!record) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+    res.status(200).send(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 export default router;
